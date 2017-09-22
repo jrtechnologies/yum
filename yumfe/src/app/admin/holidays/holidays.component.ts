aa9bfdba5c382f12238cdbf60e6f9df3e555c03e
Konstantinos Venizelos <k.venizelos@chq.jrtechnologies.com>
2017-09-22 18:00:16 +0300

    Holidays

--- yumfe/src/app/admin/holidays/holidays.component.ts
+++ yumfe/src/app/admin/holidays/holidays.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { addYears, subYears, addDays, startOfMonth, endOfMonth, getMonth, getYear, isToday, isValid } from 'date-fns';
-import { CalendarDateFormatter } from 'angular-calendar';
-import {CustomDateFormatter} from './custom-date-formatter.provider';
+import { CalendarDateFormatter, CalendarMonthViewDay } from 'angular-calendar';
+import { CustomDateFormatter } from './custom-date-formatter.provider';
 import { Observable, BehaviorSubject } from 'rxjs/Rx';
 
 @Component({
@@ -13,7 +13,8 @@ import { Observable, BehaviorSubject } from 'rxjs/Rx';
       provide: CalendarDateFormatter,
       useClass: CustomDateFormatter
     }
-  ]
+  ],
+  encapsulation: ViewEncapsulation.None
 })
 export class HolidaysComponent implements OnInit {
 
@@ -23,35 +24,72 @@ export class HolidaysComponent implements OnInit {
 
   // calendar properties  https://mattlewis92.github.io/angular-calendar/
   public view: string = 'month';
-  public viewYear:  BehaviorSubject<Date> = new BehaviorSubject<Date>(new Date()); 
+  public viewYear: BehaviorSubject<Date> = new BehaviorSubject<Date>(new Date());
   public locale: string = 'en';
   public yearMonths: Date[] = [];
+
+
+  //public holidays: CalendarMonthViewDay[] = [];
+  public holidays: Map<Date, CalendarMonthViewDay> = new Map<Date, CalendarMonthViewDay>();
   constructor() { }
 
-  ngOnInit() {      
+  ngOnInit() {
     this.viewYear.subscribe(date => {
-      this.buildMonths( date.getFullYear());
-  })
+      this.buildMonths(date.getFullYear());
+    })
  
-    
   }
 
-  ngAfterViewInit() {  }
+  ngAfterViewInit() { }
 
-  buildMonths(year: number){
-    this.yearMonths= [];
-    for(let i =0; i<12; i++){
-        this.yearMonths.push(new Date(year,i));
-      } 
-      console.log("build");
+  buildMonths(year: number) {
+
+    for (let i = 0; i < 12; i++) {
+      this.yearMonths[i] = new Date(year, i);
+    }
+    console.log("build");
   }
 
-  prevYear(){
+  prevYear() {
     this.viewYear.next(subYears(this.viewYear.getValue(), 1));
   }
 
-  nextYear(){
+  nextYear() {
     this.viewYear.next(addYears(this.viewYear.getValue(), 1));
   }
 
+  clickedDate(day: CalendarMonthViewDay, monthDate: Date) {
+
+    if (monthDate.getMonth() !== day.date.getMonth()) {
+      return;
+    }
+
+    let holiday: CalendarMonthViewDay = this.holidays.get(day.date);
+
+    if (!holiday) {
+      day.cssClass = 'cal-day-selected';
+      this.holidays.set(day.date, day);
+    } else {
+      delete day.cssClass;
+      this.holidays.delete(day.date);
+    }
+
+    console.log(this.holidays);
+  }
+
+  beforeMonthViewRender({ body }: { body: CalendarMonthViewDay[] }): void {
+    body.forEach(day => {
+
+      let holiday: CalendarMonthViewDay = this.holidays.get(day.date);
+
+      if (holiday) {
+        day.cssClass = 'cal-day-selected';
+
+      }
+
+
+    });
+  }
+
 }
+ 
