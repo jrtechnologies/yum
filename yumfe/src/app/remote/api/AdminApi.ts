4a2882ef6e12ca47928aedfe6656c0550d72f4a4
Christos Lazos <c.lazos@chq.jrtechnologies.com>
2017-10-03 15:01:06 +0300

    Search users in admin home page, closes jrtechnologies/yum#50

--- yumfe/src/app/remote/api/AdminApi.ts
+++ yumfe/src/app/remote/api/AdminApi.ts
@@ -94,14 +94,15 @@ export class AdminApi {
      * @param size Request pagination size / num of users
      * @param orderBy Request orderBy filter
      * @param orderDirection Request orderBy filter
+     * @param lastName Request search term
      */
-    public usersGet(page?: string, size?: string, orderBy?: string, orderDirection?: string, extraHttpRequestParams?: any): Observable<models.UsersPage> {
-        return this.usersGetWithHttpInfo(page, size, orderBy, orderDirection, extraHttpRequestParams)
+    public usersGet(page?: string, size?: string, orderBy?: string, orderDirection?: string, lastName?: string, extraHttpRequestParams?: any): Observable<models.UsersPage> {
+        return this.usersGetWithHttpInfo(page, size, orderBy, orderDirection, lastName, extraHttpRequestParams)
             .map((response: Response) => {
                 if (response.status === 204) {
                     return undefined;
                 } else {
-                    return response.json();
+                    return response.json() || {};
                 }
             });
     }
@@ -398,33 +399,29 @@ export class AdminApi {
      * @param orderBy Request orderBy filter
      * @param orderDirection Request orderBy filter
      */
-    public usersGetWithHttpInfo(page?: string, size?: string, orderBy?: string, orderDirection?: string, extraHttpRequestParams?: any): Observable<Response> {
-        const path = this.basePath + `/users`;
+    public usersGetWithHttpInfo(page?: string, size?: string, orderBy?: string, orderDirection?: string, lastName?: string, extraHttpRequestParams?: any): Observable<Response> {
+        const path = this.basePath + '/users';
 
         let queryParameters = new URLSearchParams();
         let headers = new Headers(this.defaultHeaders.toJSON()); // https://github.com/angular/angular/issues/6845
         if (page !== undefined) {
-
             queryParameters.set('page', <any>page);
-
         }
 
         if (size !== undefined) {
-
             queryParameters.set('size', <any>size);
-
         }
 
         if (orderBy !== undefined) {
-
             queryParameters.set('orderBy', <any>orderBy);
-
         }
 
         if (orderDirection !== undefined) {
-
             queryParameters.set('orderDirection', <any>orderDirection);
+        }
 
+        if (lastName !== undefined) {
+            queryParameters.set('lastName', <any>lastName);
         }
 
         // to determine the Content-Type header
@@ -444,9 +441,9 @@ export class AdminApi {
         let requestOptions: RequestOptionsArgs = new RequestOptions({
             method: RequestMethod.Get,
             headers: headers,
-            search: queryParameters
+            search: queryParameters,
+            withCredentials:this.configuration.withCredentials
         });
-
         // https://github.com/swagger-api/swagger-codegen/issues/4037
         if (extraHttpRequestParams) {
             requestOptions = (<any>Object).assign(requestOptions, extraHttpRequestParams);
@@ -883,7 +880,7 @@ export class AdminApi {
 
     /**
      * get holidays by year
-     * @param year 
+     * @param year
      */
     public globalsettingsHolidaysYearGet(year: number, extraHttpRequestParams?: any): Observable<Date[]> {
         return this.globalsettingsHolidaysYearGetWithHttpInfo(year, extraHttpRequestParams)
@@ -898,7 +895,7 @@ export class AdminApi {
 
     /**
      * set holidays by year
-     * @param year 
+     * @param year
      * @param holidays The holidays to set
      */
     public globalsettingsHolidaysYearPost(year: number, holidays: string[], extraHttpRequestParams?: any): Observable<{}> {
@@ -914,9 +911,9 @@ export class AdminApi {
 
 
     /**
-     * 
+     *
      * get holidays by year
-     * @param year 
+     * @param year
      */
     public globalsettingsHolidaysYearGetWithHttpInfo(year: number, extraHttpRequestParams?: any): Observable<Response> {
         const path = this.basePath + '/globalsettings/holidays/${year}'
@@ -957,9 +954,9 @@ export class AdminApi {
     }
 
     /**
-     * 
+     *
      * set holidays by year
-     * @param year 
+     * @param year
      * @param holidays The holidays to set
      */
     public globalsettingsHolidaysYearPostWithHttpInfo(year: number, holidays: string[], extraHttpRequestParams?: any): Observable<Response> {
